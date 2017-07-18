b0b64c59a43f3fb5ccb341b892cc56ae96122191
ekfkfl <shbogu@naver.com>
2017-07-18 16:09:12 +0900

    성훈 자동로그인 끝

--- app/src/main/java/dvorak/kosta/com/dothing_mobile/activity/SplashActivity.java
+++ app/src/main/java/dvorak/kosta/com/dothing_mobile/activity/SplashActivity.java
@@ -1,18 +1,24 @@
 package dvorak.kosta.com.dothing_mobile.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import dvorak.kosta.com.dothing_mobile.MainActivity;
 import dvorak.kosta.com.dothing_mobile.R;
+import dvorak.kosta.com.dothing_mobile.network.LoginNetworkTask;
 
 public class SplashActivity extends AppCompatActivity {
     int checkstate = 0;
@@ -58,12 +64,25 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 
+                SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                String id = auto.getString("LoginId",null);
+                String password = auto.getString("LoginPassword",null);
 
+                if(id != null && password !=null){
 
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                    Map<String,String> map = new HashMap<String, String>();
+                    map.put("email",id);
+                    map.put("password",password);
+                    map.put("token", FirebaseInstanceId.getInstance().getToken());
+
+                    LoginNetworkTask networkTask = new LoginNetworkTask(SplashActivity.this);
+                    networkTask.execute(map);
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                }
             }
         }, 2000);
 
