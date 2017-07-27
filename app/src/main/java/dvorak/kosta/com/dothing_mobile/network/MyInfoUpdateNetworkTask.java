a5ce7dab697446d6c3434b9793c7bf8fa55299c8
crw12 <crw12@naver.com>
2017-07-27 18:15:14 +0900

    no message

--- app/src/main/java/dvorak/kosta/com/dothing_mobile/network/MyInfoUpdateNetworkTask.java
+++ app/src/main/java/dvorak/kosta/com/dothing_mobile/network/MyInfoUpdateNetworkTask.java
@@ -79,8 +79,8 @@ public class MyInfoUpdateNetworkTask extends AsyncTask<Map<String, Object>,Integ
         //http 요청 준비 작업
         dvorak.kosta.com.dothing_mobile.HttpClient.Builder http = new dvorak.kosta.com.dothing_mobile.HttpClient.Builder("POST", ConstantUtil.ipAddr + "androidMember/myinfoUpdate");
         dvorak.kosta.com.dothing_mobile.HttpClient post = http.create();
-        http.addAllParameters(params[0]);
-        http.addOrReplace();
+        /*http.addAllParameters(params[0]);
+        http.addOrReplace();*/
         post.request();
 
         //응답 상태 코드 가져오기
